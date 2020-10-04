599d5e23c6b465d339b5cf89127fc34f8efba08e
Claxl <claxlxl@gmail.com>
2020-10-05 00:12:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { create, Client } = require('@open-wa/wa-automate')
-const welcome = require('./lib/welcome')
 const msgHandler = require('./msgHndlr')
 const options = require('./options')
 
